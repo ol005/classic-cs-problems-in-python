19be5f700bae6e57074f7d882daf47c92d1409a1
oliver <189471515+ol005@users.noreply.github.com>
2025-07-20 22:25:08 -0400

    question 2 exercises

--- ch1/nucl.py
+++ ch1/nucl.py
@@ -1,30 +1,64 @@
 # string to bit gene nucleotide compression
 # ch1 1.2
 import sys
+from typing import Generator
 
+# implementing wrapper around int to handle bitstrings
+# question 2 in 1.7 excercises
+class BitString:
+    def __init__(self, start: int) -> None:
+        self._bit_string: int = start
+    
+    def lshift(self, n: int) -> None:
+        self._bit_string <<= n
+    
+    def append(self, bits: int) -> None:
+        self._bit_string |= bits
+
+    def __sizeof__(self) -> int:
+        return sys.getsizeof(self._bit_string)
+    
+    def __len__(self) -> int:
+        return self._bit_string.bit_length()-1
+    
+    def __iter__(self) -> Generator[int, None, None]:
+        for i in range(0, len(self), 2):
+            yield self._bit_string >> i & 0b11
+
+    def __getitem__(self, index: int) -> int:
+        if index < 0 or index > len(self):
+            raise IndexError
+        return self._bit_string >> index & 0b11
+
+    def __str__(self) -> str:
+        return bin(self._bit_string)
+    
+#rewrite compressed gene to use BitString class
 class CompressedGene:
     def __init__(self, gene: str) -> None:
         self._compress(gene)
     
+    def get_bstr(self) -> BitString:
+        return self._b_str
+    
     def _compress(self, gene: str) -> None:
-        self.bit_string: int = 1
+        self._b_str: BitString = BitString(1)
         for nucleotide in gene.upper():
-            self.bit_string <<= 2
+            self._b_str.lshift(2)
             if nucleotide == 'A':
-                self.bit_string |= 0b00
+                self._b_str.append(0b00)
             elif nucleotide == 'C':
-                self.bit_string |= 0b01
+                self._b_str.append(0b01)
             elif nucleotide == 'G':
-                self.bit_string |= 0b10
+               self._b_str.append(0b10)
             elif nucleotide == 'T':
-                self.bit_string |= 0b11
+              self._b_str.append(0b11)
             else:
                 raise ValueError(f"Invalid Nucleaotide:{nucleotide}")
 
     def decompress(self) -> str:
         gene: str = ""
-        for i in range(0, self.bit_string.bit_length()-1, 2):
-            bits: int = self.bit_string >> i & 0b11
+        for bits in self._b_str:
             if bits == 0b00:
                 gene += 'A'
             elif bits == 0b01:
@@ -43,6 +77,5 @@ class CompressedGene:
 txt = "TAGGGATTAACCGTTATATATATATAGCCATGGATCGATTATATAGGGATTAACCGTTATATATATATAGCCATGGATCGATTATA" * 100
 gene = CompressedGene(txt)
 print(f"size of str: {sys.getsizeof(txt)}")
-#print(bin(gene.bit_string))
-print(f"size of bit str: {sys.getsizeof(gene.bit_string)}")
-#print(gene)
+print(f"size of bit str: {sys.getsizeof(gene.get_bstr())}")
+print(gene)
